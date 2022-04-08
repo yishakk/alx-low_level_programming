92607cab34d718248a2925c17d825392b5e1136f
yishakk <yishakkibru@gmail.com>
2022-04-08 13:18:34 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,12 +6,13 @@
  * main - entry point
  * 
  * Description: Prints a number and it's last digit along with whether its
+ *
  * last digit is 0, greater than 5, or less than 6 and not 0.
+ *
  * Return: 0
  */
 int main(void)
 {
-
 int n;
 
 srand(time(0));
