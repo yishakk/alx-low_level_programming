2d6e69400f79c077c024c85ac3a849dcab27be98
yishakk <yishakkibru@gmail.com>
2022-05-12 11:11:46 -0700

    variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef VARIADIC_FUNCTIONS_H_
+#define VARIADIC_FUNCTIONS_H_
 #include <stdarg.h>
 
 int _putchar(char c);
@@ -7,19 +7,20 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void print_a_char(char *separator, va_list args);
-void print_a_integer(char *separator, va_list args);
-void print_a_float(char *separator, va_list args);
-void print_a_char_ptr(char *separator, va_list args);
+
 
 /**
- * struct format_types - Struct format_types
- * @identifier: The conversion specifier
- * @f: The function pointer
+ * struct format_struct - format-type class
+ * @format: format identifier
+ * @fmt_print_func: pointer to function that prints in the format of @format
  */
-typedef struct format_types
+
+struct format_struct
 {
-	char *identifier;
-	void (*f)(char *separator, va_list args);
-} f_dt;
+	char format;
+	void (*fmt_print_func)(va_list *arg_list);
+};
+
+typedef struct format_struct format_struct_ptr;
+
 #endif
