4cac926893a879eac3a1af0d4e310db151f6fd24
yishakk <yishakkibru@gmail.com>
2022-05-23 11:04:24 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,15 +9,15 @@
 
 size_t print_listint(const listint_t *h)
 {
-	const listint_t *tp;
-	unsigned int cntr = 0;
+const listint_t *tp;
+unsigned int cntr = 0;
 
-	tp = h;
-	while(tp)
-	{
-		printf("%d\n", tp->n);
-		cntr++;
-		tp = tp->next;
-	}
-	return (cntr);
+tp = h;
+while (tp)
+{
+printf("%d\n", tp->n);
+cntr++;
+tp = tp->next;
+}
+return (cntr);
 }
